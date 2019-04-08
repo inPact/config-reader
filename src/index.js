9b6f0ce33f8b4041808fcedf7e658798d9003767
joniba <jbenami1@gmail.com>
2019-04-08 19:07:09 +0300

    default env

--- src/index.js
+++ src/index.js
@@ -3,22 +3,24 @@ const floatRegex = /^\d+\.?\d*$/;
 const path = require('path');
 
 module.exports = {
-    read({ env, dirPath, defaultConfigPath } = {}) {
-        let root = path.resolve('./');
-        console.log(`========== root: ${JSON.stringify(root, null, 2)} ==========`);
-
-        let env = process.env.NODE_ENV === 'test'
+    get env() {
+        return process.env.NODE_ENV === 'test'
             ? process.env.TEST_ENV || process.env.NODE_ENV
             : process.env.NODE_ENV || 'local';
+    },
+
+    read({ env, dirPath, defaultConfigPath } = {}) {
+        env = env || this.env;
 
         let config = _.merge(require(defaultConfigPath),
-            require(dirPath + env),
+            require(path.join(dirPath, env)),
             (a, b) => {
                 if (b && b.__override)
                     return b;
             });
 
         applyPathConfig(config, process.env);
+        return config;
     }
 };
 
